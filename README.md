87eca7c6adde3dadc9da4b8492e471d3b9def2f3
Dan Shai <danshai2013@gmail.com>
2018-09-03 21:02:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1 align="center">
+  --testin---!!
+</h1>
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = (props) => (
+    <footer id="footer">
+        <div className="inner">
+            <ul className="icons">
+                <li><a href="#" className="icon alt fa-twitter"><span className="label">No Twitter</span></a></li>
+                <li><a href="#" className="icon alt fa-facebook"><span className="label">No Facebook</span></a></li>
+                <li><a href="#" className="icon alt fa-instagram"><span className="label">No Instagram</span></a></li>
+                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="#" className="icon alt fa-linkedin"><span className="label">No LinkedIn</span></a></li>
+            </ul>
+        </div>
+    </footer>
+)
+
+export default Footer
